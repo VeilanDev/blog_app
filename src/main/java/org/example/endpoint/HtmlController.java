a69185ec3972b03d0d81f3085b48480d361c8ade
VeilanDev <playgame2018@yandex.ru>
2026-07-08 22:27:47 +0300

    Сделал основу для страницы с постами

--- src/main/java/org/example/endpoint/HtmlController.java
+++ src/main/java/org/example/endpoint/HtmlController.java
@@ -1,13 +1,24 @@
 package org.example.endpoint;
 
+import org.example.entity.User;
+import org.example.repository.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HtmlController {
     private static final Logger log = LoggerFactory.getLogger(HtmlController.class);
+
+    private final UserRepository userRepository;
+
+    public HtmlController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @GetMapping("/login")
     public String toLogin() {
@@ -15,7 +26,20 @@ public class HtmlController {
     }
 
     @GetMapping("/home")
-    public String toHome() {
+    public String toHome(
+            Model model,
+            Authentication authentication
+    ) {
+        String login = authentication.getName();
+        userRepository.findByLogin(login).ifPresent(
+                user -> {
+                    model.addAttribute("userLogin", login);
+                    model.addAttribute("userName", user.getName());
+
+                }
+        );
+
+
         return "home";
     }
 
